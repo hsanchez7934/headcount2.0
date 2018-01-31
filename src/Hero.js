01dc5a6d7bfd7074597fd3637dd2988ce941431d
Hector Sanchez <hsanchez7934@gmail.com>
2018-01-30 23:47:08 -0700

    Add description paragraph to hero component

--- src/Hero.js
+++ src/Hero.js
@@ -4,6 +4,10 @@ import './styles/Hero.css';
 const Hero = () => (
   <div className='hero'>
     <h1>HeadCount 2.0</h1>
+    <p id='app-text'>
+      Select two cards to create comparison card. Comparison card
+      will display the compared average of the school school districts.
+    </p>
   </div>
 );
 
